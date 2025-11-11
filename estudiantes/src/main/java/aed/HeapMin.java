f03e4910596d111d7244da12cf05b92940fb16f7
AleME2002 <martinezevansalejo@gmail.com>
2025-11-11 09:10:29 -0300

    Pase el heap

--- estudiantes/src/main/java/aed/HeapMin.java
+++ estudiantes/src/main/java/aed/HeapMin.java
@@ -7,122 +7,162 @@ public class heapMin {
     private int tamaño;         // cuántos hay realmente (por si desencolamos gente)
     private int capacidad;      // max. estudiantes que puede tener el heap (siempre = E)
 
+
+//------------------------------------------------------------------------Handle--------------------------------------------------------
+
+
     public class Handle {
         private int id; 
         private heapMin miHeap; 
 
         private Handle(int id, heapMin padre) { 
-            this.id = id; 
-            this.miHeap = padre; 
+            this.id = id;                                           // O(1)
+            this.miHeap = padre;                                    // O(1)
         }
 
         public int id() {
-            return id; 
+            return id;                                              // O(1)
         }
 
         public int nota() {
-            return miHeap.notas[id];
+            return miHeap.notas[id];                                // O(1)
         }
 
-        // Complejidad = O(log E)
         public void actualizarNota(int nuevaNota) {
-            miHeap.actualizarNotaDesdeHandle(id, nuevaNota);    // O(log E)
+            miHeap.actualizarNotaDesdeHandle(id, nuevaNota);        // O(log E)
         }
     }
 
-    // CONSTRUCTOR - usamos heapify para crear el heap en O(E)
+
+//------------------------------------------------------------------------Constructor--------------------------------------------------
+    
+
     public heapMin(int cantEstudiantes, int[] notasIniciales) {
-        this.capacidad = cantEstudiantes; 
-        this.heap = new int[capacidad]; 
-        this.posiciones = new int[capacidad]; 
-        this.notas = new int[capacidad]; 
-        this.tamaño = capacidad; // al principio, todos están en el heap 
+        this.capacidad = cantEstudiantes;                       // O(1)
+        this.tamaño = cantEstudiantes;                          // O(1)
+        this.heap = new int[cantEstudiantes];                   // O(E)
+        this.posiciones = new int[cantEstudiantes];             // O(E)
+        this.notas = new int[cantEstudiantes];                  // O(E)
+        this.armarHeap(notasIniciales);                         // O(E)
+    } // Complejidad: O(E)
 
-        armarHeap(notasIniciales);  // O(E)
-        usarHeapify();              // O(E)
-    }
-
-    // NOTAS INICIALES SON TODOS 0 AL INICIO. POR AHI NO ES NECESARIO PASAR LAS NOTAS INICIALES
-    // Complejidad = O(E)
     private void armarHeap(int[] notasIniciales) {
-        for (int i = 0; i < capacidad; i++) {   // E iteraciones
-            heap[i] = i;                        // O(1)
-            posiciones[i] = i;                  // O(1)
-            notas[i] = notasIniciales[i];       // O(1)
+        for (int i = 0; i < capacidad; i++) {         // Recorre todos los estudiantes                        // O(E)
+            heap[i] = i;                              // Guarda el ID del estudiante                          // O(1)
+            posiciones[i] = i;                        // Registra en que posicion del heap esta ese id        // O(1)
+            notas[i] = 0;                             // Asigna la nota inicial de ese estudiant              // O(1)
         }
-    }
+    } // Complejidad: O(E)
+    
+    /*
+    Primero se inicializan todas las estructuras internas del heap.
+    Luego, en armarHeap() se cargan los IDs de los estudiantes, como todos comienzan con un examen vacío, 
+    el heap ya cumple el invariante y no es necesario aplicar Heapify().
+    */
 
-    /**
-     * ¿Heapify? - O(E)
-     * Hacemos siftDown por cada uno de los niveles, "yendo hacia atrás", hasta la raíz. 
-     * 
-     * 1. Empezamos desde el último padre (el nodo más abajo que tiene dos hijos)
-     * 2. Hacemos siftDown a este padre y a todos los padres de arriba.
-     * 3. Subimos de nivel hasta llegar a la raiz.
-     */
 
-    // Complejidad = O(E)
-    private void usarHeapify() {
-        int ultimoPadre = encontrarUltimoPadre();
-        usarSiftDownDesde(ultimoPadre); 
-    }
+//------------------------------------------------------------------------Encolar--------------------------------------------------
 
-    private int encontrarUltimoPadre() {
-        int ultimoElemento = tamaño -1; 
-        int padreDelUltimo = (ultimoElemento - 1)/2;
-        return padreDelUltimo;
-    }
 
-    private void usarSiftDownDesde(int empezarDesde) {
-        for (int posActual = empezarDesde; posActual >= 0; posActual--) {
-            siftDown(posActual);
-        }
-    }
+    public void encolar(int id) {
+        heap[tamaño] = id;              // Inserta el nuevo estudiante en la última posición libre del heap                       // O(1)
+        posiciones[id] = tamaño;        // Registra en qué posición del heap quedó almacenado ese estudiante                      // O(1)
+        tamaño++;                       // Aumenta el tamaño del heap, ya que ahora contiene un elemento más                      // O(1)
+        siftUp(tamaño - 1);             // Reacomoda el nuevo elemento hacia arriba si su nota es menor que la de su padre        // O(log E)
+    } // Complejiidad: O(log E)
 
-    // actualizar notas con handle
-    // Complejidad = O(log E)
+
+//------------------------------------------------------------------------Desencolar--------------------------------------------------
+
+
+    public int desencolar() {
+        int peorId = heap[0];               // Guarda el ID del estudiante con peor nota (raíz del heap)                      // O(1)
+        intercambiar(0, tamaño - 1);        // Intercambia la raíz con el último elemento del heap                            // O(1)
+        tamaño--;                           // Reduce el tamaño del heap, "eliminando" el último elemento (el peor)           // O(1)
+        siftDown(0);                        // Reacomoda el nuevo elemento en la raíz para restaurar el orden del heap        // O(log E)
+        return peorId;                      // Devuelve el ID del estudiante con peor nota                                    // O(1)
+    } // Complejidad: O(log E)
+
+
+//------------------------------------------------------------------------Actualizar nota--------------------------------------------------
+
+
     private void actualizarNotaDesdeHandle(int id, int nuevaNota) { 
-        int notaVieja = notas[id]; 
-        notas[id] = nuevaNota;
-        int pos = posiciones[id]; 
+        int notaVieja = notas[id];          // Guarda la nota actual del estudiante antes de actualizarla           // O(1)
+        notas[id] = nuevaNota;              // Asigna la nueva nota al estudiante en el arreglo de notas            // O(1)
+        int pos = posiciones[id];           // Obtiene la posición actual del estudiante dentro del heap            // O(1)
+        if (nuevaNota < notaVieja)          // Si la nueva nota es menor que la anterior                            // O(1)
+            siftUp(pos);                    // Sube al estudiante en el heap hasta restaurar el orden               // O(log E)
+        else if (nuevaNota > notaVieja)     // Si la nueva nota es mayor que la anterior                            // O(1) 
+            siftDown(pos);                  // Baja al estudiante en el heap hasta restaurar el orden               // O(log E)
+    } // Complejidad: O(log E)
 
-        if (nuevaNota < notaVieja) { 
-            siftUp(pos);                        // O(log E)
-        } else if (nuevaNota > notaVieja) { 
-            siftDown(pos);                      // O(log E)
-        }
-    }
+    /*
+    Este método actualiza la nota de un estudiante.
+    Luego determina si esa nota subio o bajo para reacomodarla en el heap.
+    */
 
-    // Complejidad = O(log E)
+//------------------------------------------------------------------------SiftUp--------------------------------------------------------------
+
+
     private void siftUp(int pos) { 
-        while (pos > 0) {                  // máximo altura del árbol = log E
-            int padre = (pos - 1)/2; 
-            
-            if (debeSubir(pos, padre)) {
-                intercambiar(pos, padre);
-                pos = padre; 
-                // seguimos viendo desde la nueva pos 
-            } else { 
-                break; // VER SI PODES USAR BREAK
-            }
-        }
-    }
+    while (pos > 0 && debeSubir(pos, (pos - 1) / 2)) {   // Recorre hacia arriba mientras no sea la raíz y el estudiante actual deba subir      // O(log E)
+        int padre = (pos - 1) / 2;                       // Calcula el índice del estudiante padre                                              // O(1)
+        intercambiar(pos, padre);                        // Intercambia el estudiante con su padre en el heap                                   // O(1)
+        pos = padre;                                     // Actualiza la posición actual al nuevo índice                                        // O(1)
+        }        
+    }   // Complejidad total: O(log E)
 
-    // Complejidad = O(log E)
+
+
+//------------------------------------------------------------------------SiftDown--------------------------------------------------------------
+
     private void siftDown(int pos) { 
         while (pos < tamaño) { 
             int izq = 2 * pos + 1; 
             int der = 2 * pos + 2;
-            int menor = encontrarMenor(pos, izq, der); 
-            
-            if (menor == pos) {
+            int menor = 0; 
+            if (izq < tamaño && debeSubir(izq, menor))
+                menor = izq;
+            else if (der < tamaño && debeSubir(der, menor))
+                menor = der;
+            else if (menor == pos)
                 break; // encontró su lugar 
-            } else {
+            else 
                 intercambiar(pos, menor);
                 pos = menor; // seguimos viendo desde la nueva pos
-            }
+        }
+    }// Complejidad = O(log E)
+
+//------------------------------------------------------------------------Heapify--------------------------------------------------------
+    
+    /*
+    ¿Heapify? - O(E)
+    Hacemos siftDown por cada uno de los niveles, "yendo hacia atrás", hasta la raíz. 
+    
+    1. Empezamos desde el último padre (el nodo más abajo que tiene dos hijos)
+    2. Hacemos siftDown a este padre y a todos los padres de arriba.
+    3. Subimos de nivel hasta llegar a la raiz.
+    */
+    // Complejidad = O(E)
+    private void Heapify() {
+        int ultimoPadre = encontrarUltimoPadre();                                           // O(1)
+        usarSiftDownDesde(ultimoPadre);                                                     // O(E)
+    }
+    private int encontrarUltimoPadre() {
+        int ultimoElemento = tamaño -1;                                                     // O(1)
+        int padreDelUltimo = (ultimoElemento - 1)/2;                                        // O(1)
+        return padreDelUltimo;                                                              // O(1)
+    }
+    private void usarSiftDownDesde(int empezarDesde) {
+        for (int posActual = empezarDesde; posActual >= 0; posActual--) {                   // O(E)           
+            siftDown(posActual);                                                            // O(1)
         }
     }
+
+
+//------------------------------------------------------------------------Extras--------------------------------------------------------
+
 
     public void intercambiar(int i, int j) { 
         int idEnI = heap[i]; 
@@ -137,58 +177,23 @@ public class heapMin {
 
     // USAR COMPARE TO ¿?
     private boolean debeSubir(int i, int j) { 
-        int idPosI = heap[i]; 
+        int idPosI = heap[i];                                                                           
         int idPosJ = heap[j]; 
         boolean res = true;
-
         // comparamos la nota. si es la misma, se desempata por menor id
         if ((notas[idPosI] < notas[idPosJ]) || (notas[idPosI] == notas[idPosJ] && idPosI < idPosJ)) { 
             res = true; 
         } else {
             res = false;
         }
-
         return res; 
     }
 
-    public int encontrarMenor(int pos, int izq, int der) { 
-        int menor = pos; 
 
-        // veamos hijo izquierdo
-        if (izq < tamaño && debeSubir(izq, menor)) {
-            menor = izq;
-        }
 
-        // ver hijo derecho
-        if (der < tamaño && debeSubir(der, menor)) {
-            menor = der;
-        }
 
-        return menor;
-    }
 
-    // PARA 4.consultarDarkWeb - sacar el peor estudiante 
-    // Complejidad = O(log E)
-    public int desencolar() {
-
-        int peorId = heap[0]; 
-
-        intercambiar(0, tamaño-1); // mandamos el peor al final 
-        tamaño--;                    // lo sacamos del heap
-        siftDown(0);            // reacomodamos desde la raiz
-
-        return peorId;
-    }
-
-    // Complejiidad = O(log E)
-    public void encolar(int id) {
-        heap[tamaño] = id; 
-        posiciones[id] = tamaño;  
-        tamaño++; 
-        siftUp(tamaño - 1);
-    }
-    
-    // Complejidad = O(1)
+        // Complejidad = O(1)
     public Handle obtenerHandle(int id) {
         Handle handleParaId = new Handle(id, this); 
         return handleParaId;
@@ -198,5 +203,4 @@ public class heapMin {
     public boolean vacio() {
         return tamaño == 0;
     }
-
 }
