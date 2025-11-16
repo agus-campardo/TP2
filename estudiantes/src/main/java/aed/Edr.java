79d065abc0107b234624414ac1f26913206bc360
AleME2002 <martinezevansalejo@gmail.com>
2025-11-16 00:05:08 -0300

    a

--- estudiantes/src/main/java/aed/Edr.java
+++ estudiantes/src/main/java/aed/Edr.java
@@ -318,7 +318,7 @@ public class Edr {
                 if (respuesta == -1){                                   // Si no respondio la pregunta, la saltea               // O(1)
                     continue;                                                                                                   // O(1)
                 }
-                if (grilla[j][respuesta] < umbral) {                    // Me fijo si respondio por debajo del 25%              // O(1)
+                if (grilla[j][respuesta] - 1 < umbral) {                // Me fijo si respondio por debajo del 25%              // O(1)
                     estudiantes[i].sospechoso = false;                  // Si lo hizo, quiere decir que no se copio             // O(1)
                     break;                                                                                                      // O(1)
                 } 
