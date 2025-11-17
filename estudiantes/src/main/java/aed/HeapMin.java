e5d3c95836133d30be78ee2da8dde0564fa44f4b
lucas-akerman <lucas.akerman@gmail.com>
2025-11-16 21:09:56 -0300

    algunos comenatarios a heapmin

--- estudiantes/src/main/java/aed/HeapMin.java
+++ estudiantes/src/main/java/aed/HeapMin.java
@@ -2,7 +2,7 @@ package aed;
 
 public class HeapMin {
      private HandleEst[] heap;          // los elementos del heap son handles con estudiantes y su psocion en el heaps  
-     private int tamaño;                // cuántos hay realmente (por si desencolamos gente)
+     private int tamaño;                // cuántos hay realmente (por si desencolamos handles)
      private int capacidad;             // max. estudiantes que puede tener el heap (siempre = E)
 
 //------------------------------------------------------------------------Constructor--------------------------------------------------
@@ -20,7 +20,7 @@ public class HeapMin {
 
     private void armarHeap(HandleEst[] estudiantes) {
         for (int i = 0; i < capacidad; i++) {                   // Recorre todos los estudiantes                                // O(E)
-            HandleEst h = estudiantes[i];                       // copio por alaising el handle que estaba en estduaintes       // O(1)
+            HandleEst h = estudiantes[i];                       // copio por alaising!! el handle que estaba en estduaintes. por lo tanto si los valores del handel cambian, cambian en ambos lados     // O(1)
             heap[i] = h;                                        // Guarda el handle del estudiante en el heap                   // O(1)
             h.cambiarPosicionEnHeap(i);                         // Registra en que posicion del heap esta ese id                // O(1)
         }
@@ -43,7 +43,7 @@ public class HeapMin {
 
         hEst.cambiarPosicionEnHeap(tamaño);
         tamaño++;                       // Aumenta el tamaño del heap, ya que ahora contiene un elemento más                      // O(1)
-        siftUp(tamaño - 1);             // Reacomoda el nuevo elemento hacia arriba si su nota es menor que la de su padre        // O(log E)
+        siftUp(tamaño - 1);             // Reacomoda el nuevo elemento hacia arriba si su prioridad es mayor que la de su padre        // O(log E)
     } // Complejiidad: O(log E)
 
 
@@ -56,7 +56,7 @@ public class HeapMin {
         intercambiar(0, tamaño - 1);        // Intercambia la raíz con el último elemento del heap                            // O(1)
         tamaño--;                           // Reduce el tamaño del heap, "eliminando" el último elemento (el peor)           // O(1)
         siftDown(0);                        // Reacomoda el nuevo elemento en la raíz para restaurar el orden del heap        // O(log E)
-        return estConPeorNota;                      // Devuelve el ID del estudiante con peor nota                                    // O(1)
+        return estConPeorNota;                      // Devuelve el estudiante con peor nota                                    // O(1)
     } // Complejidad: O(log E)
 
 
@@ -68,13 +68,13 @@ public class HeapMin {
                                                         // la nota ya esta actualizada. lo unico que nos falta,
                                                         // es actualizar la posicion del handel en el heap.
 
-        siftUp(pos);                    // Sube al estudiante en el heap hasta restaurar el orden               // O(log E)
-        siftDown(pos);                  // Baja al estudiante en el heap hasta restaurar el orden               // O(log E)
+        siftUp(pos);                    // Sube al handel del estudiante en el heap hasta restaurar el orden               // O(log E)
+        siftDown(pos);                  // Baja al handel del estudiante en el heap hasta restaurar el orden               // O(log E)
     } // Complejidad: O(log E)
 
     /*
-    Este método actualiza la nota de un estudiante.
-    Luego determina si esa nota subio o bajo para reacomodarla en el heap.
+    Este método enccuentra la poosicion del heap del estudiante que cambio su nota o su estado de entrega.
+    Luego reacomoda el handel en el heap por si su cambio del estudiante involucro un cambio en su prioridad.
     */
 
 //------------------------------------------------------------------------SiftUp--------------------------------------------------------------
@@ -83,8 +83,8 @@ public class HeapMin {
 
     private void siftUp(int pos) { 
 
-        while (pos > 0 && debeSubir(pos, (pos - 1) / 2)) {   // Recorre hacia arriba mientras no sea la raíz y el estudiante actual deba subir      // O(log E)
-            int padre = (pos - 1) / 2;                       // Calcula el índice del estudiante padre                                              // O(1)
+        while (pos > 0 && debeSubir(pos, (pos - 1) / 2)) {   // Recorre hacia arriba mientras no sea la raíz y el handel del estudiante actual deba subir      // O(log E)
+            int padre = (pos - 1) / 2;                       // Calcula el índice del handel del estudiante padre                                              // O(1)
             intercambiar(pos, padre);                        // Intercambia el estudiante con su padre en el heap                                   // O(1)
             pos = padre;                                     // Actualiza la posición actual al nuevo índice                                        // O(1)
         }        
@@ -122,17 +122,17 @@ public class HeapMin {
         HandleEst hEnI = heap[i]; 
         HandleEst hEnJ = heap[j]; 
         
-        // intercambiamos ids en el heap y actualizamos las posiciones
+        // intercambiamos handels en el heap y actualizamos las posiciones
         heap[i] = hEnJ;
         heap[j] = hEnI;
         hEnI.cambiarPosicionEnHeap(j);
         hEnJ.cambiarPosicionEnHeap(i);
-    }
+    }//O(1)
 
 
 
 
-    // USAR COMPARE TO ¿?
+    
     private boolean debeSubir(int hijo, int padre) { 
         HandleEst hHijo = heap[hijo];                                                                           
         HandleEst hPadre = heap[padre]; 
@@ -154,7 +154,7 @@ public class HeapMin {
 
    
 
-    // TAL VEZ SIRVE ¿? BORRAR SI NO
+    
     public boolean vacio() {
         return tamaño == 0;
     }
